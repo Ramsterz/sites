9205f2b7b25b0e3ccacfcbbecc8a61b5c50070d6
Ilan Gur <ilan@ramsterz.com>
2017-03-14 16:24:18 +0200

    Add feelsyoung DB

---
+++ feelsyou/db/dumps/Dump/feelsyou_wp_wflogins.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: feelsyou
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `wp_wflogins`
+--
+
+DROP TABLE IF EXISTS `wp_wflogins`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `wp_wflogins` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `hitID` int(11) DEFAULT NULL,
+  `ctime` double(17,6) unsigned NOT NULL,
+  `fail` tinyint(3) unsigned NOT NULL,
+  `action` varchar(40) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `userID` int(10) unsigned NOT NULL,
+  `IP` binary(16) DEFAULT NULL,
+  `UA` text,
+  PRIMARY KEY (`id`),
+  KEY `k1` (`IP`,`fail`),
+  KEY `hitID` (`hitID`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `wp_wflogins`
+--
+
+LOCK TABLES `wp_wflogins` WRITE;
+/*!40000 ALTER TABLE `wp_wflogins` DISABLE KEYS */;
+INSERT INTO `wp_wflogins` VALUES (1,30,1479221770.279246,1,'loginFailInvalidUsername','feelsyou',0,'\0\0\0\0\0\0\0\0\0\0ÿÿ¼xšZ','Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.87 Safari/537.36'),(2,38,1479222883.972695,0,'loginOK','admin',1,'\0\0\0\0\0\0\0\0\0\0ÿÿT_%L','Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.71 Safari/537.36'),(3,827,1479245714.363995,0,'logout','admin',1,'\0\0\0\0\0\0\0\0\0\0ÿÿ\ÄÏ»','Mozilla/5.0 (Windows NT 6.3; WOW64; rv:49.0) Gecko/20100101 Firefox/49.0');
+/*!40000 ALTER TABLE `wp_wflogins` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2017-03-14 16:23:02
